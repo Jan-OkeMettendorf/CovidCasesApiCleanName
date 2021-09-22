e6b98423c3b3a433682a55150c2a25b520360d45
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-09-22 22:00:09 +0200

    Add project

---
+++ src/test/java/com/example/covidcasesapi/CovidCasesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.covidcasesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidCasesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
